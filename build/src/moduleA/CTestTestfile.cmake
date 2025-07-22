795d11268344245c735d1abb5d9db699cf83e905
gou <19513317149@163.com>
2025-07-22 17:01:10 +0800

    【fix】将不必要的中间运行文件删除

--- build/src/moduleA/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/cmakelearn/src/moduleA
-# Build directory: D:/cmakelearn/build/src/moduleA
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/third_party/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/cmakelearn/third_party/googletest/googlemock
-# Build directory: D:/cmakelearn/build/third_party/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
