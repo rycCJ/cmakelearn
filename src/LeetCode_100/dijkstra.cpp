e15de62fa9774dd9cd2e79741e1c5fc5b6c1e368
rycCJ <ryc1819009649@outlook.com>
2026-04-16 21:04:25 +0800

    dijkstram完成

--- src/LeetCode_100/dijkstra.cpp
+++ src/LeetCode_100/dijkstra.cpp
@@ -8,6 +8,8 @@ using namespace std;
 // 定义：{距离, 顶点编号}
 typedef pair<int, int> PII;
 
+/*
+---------------AI给的dijkstra算法模板-----------------
 vector<int> dijkstra(int n, int start, const vector<vector<pair<int, int>>>& adj) {
     // 1. 初始化距离数组，全部设为无穷大
     vector<int> dist(n + 1, INT_MAX);
@@ -39,7 +41,31 @@ vector<int> dijkstra(int n, int start, const vector<vector<pair<int, int>>>& adj
     }
     return dist; // 返回从 start 到所有点的最短距离
 }
-int main() {
+*/
+/*--------------自己写的dijkstra算法模板-----------------*/
+vector<int> dijkstra2(int n, int start, const vector<vector<pair<int, int>>>& adj) {
+    vector<int> dist(n+1,INT_MAX);//<距离，顶点编号>
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+    pq.push({0,1});
+    dist[start] = 0;
+    while(!pq.empty()){
+        int min_dist = pq.top().first;
+        int current_node = pq.top().second;
+        pq.pop();
+        if(min_dist>dist[current_node]) continue;  //如果算出来的最小值（进while得到）比当前记录的值还大，说明没有更新成功，则代表已经结束
+        for (const pair<int,int>& neibor:adj[current_node]){
+            int node = neibor.first;
+            int d = neibor.second;
+            if(dist[current_node]+d<dist[node]){
+                dist[node] = dist[current_node]+d;
+                pq.push({dist[node],node});
+            }      
+        }
+    }
+    return dist;
+}
+int main(){
+
     // 示例图：5个顶点，边权如下
     int n = 5;
     vector<vector<pair<int, int>>> adj(n + 1);
@@ -54,7 +80,7 @@ int main() {
     adj[5].push_back({4, 6});
 
     int start = 1;
-    vector<int> shortestDistances = dijkstra(n, start, adj);
+    vector<int> shortestDistances = dijkstra2(n, start, adj);
 
     // 输出从 start 到每个顶点的最短距离
     for (int i = 1; i <= n; ++i) {
@@ -64,7 +90,5 @@ int main() {
             std::cout << "Distance from " << start << " to " << i << ": " << shortestDistances[i] << std::endl;
         }
     }
-
-
     return 0;
 }
